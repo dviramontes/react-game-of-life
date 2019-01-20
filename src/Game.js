9f5ad2484417e4071cdb2e3ee48887cb2ac033c2
David A. Viramontes <dviramontes@gmail.com>
2019-01-20 16:45:23 -0700

    refactor: <Cell/> functional component

--- src/Game.js
+++ src/Game.js
@@ -5,20 +5,14 @@ const WIDTH = 800;
 const HEIGHT = 600;
 const CELL_SIZE = 20;
 
-class Cell extends React.Component {
-	render() {
-		const {x, y} = this.props;
-		return (
-			<div className="Cell"
-			     style={{
-				     left: `${CELL_SIZE * x + 1}px`,
-				     top: `${CELL_SIZE * y + 1}px`,
-				     width: `${CELL_SIZE - 1}px`,
-				     height: `${CELL_SIZE - 1}px`,
-			     }}/>
-		)
-	}
-}
+const Cell = ({ x, y }) => (
+	<div className="Cell"
+	     style={{
+		     left: `${CELL_SIZE * x + 1}px`,
+		     top: `${CELL_SIZE * y + 1}px`,
+		     width: `${CELL_SIZE - 1}px`,
+		     height: `${CELL_SIZE - 1}px`,
+	     }}/>)
 
 class Game extends React.Component {
 	constructor() {
